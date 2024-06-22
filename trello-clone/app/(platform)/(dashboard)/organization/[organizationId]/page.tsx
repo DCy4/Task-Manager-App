97a96edbde80f7face30cc4052484a6c39022930
DCy4 <davidacalle@gmail.com>
2024-06-22 19:30:25 -0400

    added dashboard/_components/navbar

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,11 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+
 const OrganizationIdPage = () => {
   return (
     <div>
-      Organization Page!
+      Organization Page
     </div>
   );
 };
